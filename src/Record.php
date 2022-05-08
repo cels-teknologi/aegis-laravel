2ecca999cae1b35fa1ac7069126e96b3c178bb6c
Stephen Dewanto <stephen.dewanto@gmail.com>
2022-05-08 08:39:33 +0700

    Feat: add code preview & probable root cause

--- src/Record.php
+++ src/Record.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Support\Jsonable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Str;
 use Throwable;
 
 class Record implements Arrayable
@@ -16,9 +17,116 @@ class Record implements Arrayable
     /** @var Throwable The throwable exception. */
     protected $exception;
 
+    /** @var array The probable root cause of exception. */
+    protected $cause;
+
     public function __construct(Throwable $exception)
     {
         $this->exception = $exception;
+        $this->cause = $this->determineCause();
+    }
+
+    public function determineCause(): array
+    {
+        $cause = [
+            'file' => $this->exception->getFile(),
+            'line' => $this->exception->getLine(),
+        ];
+        $ignores = [];
+        $finished = false;
+
+        foreach (config('aegis.ignore') as $ignore) {
+            $ignores[] = base_path($ignore);
+        }
+
+        foreach ($this->exception->getTrace() as $trace) {
+            $path = $trace['file'];
+            foreach ($ignores as $ignore) {
+                if (Str::startsWith($path, $ignore)) {
+                    continue;
+                }
+
+                $cause = [
+                    'file' => $path,
+                    'line' => $trace['line'],
+                ];
+                $finished = true;
+                break;
+            }
+
+            if ($finished) {
+                break;
+            }
+        }
+
+        return $cause;
+    }
+
+    /**
+     * Get the exception identifier to determine uniqueness.
+     *
+     * @return array
+     */
+    public function generateCodePreview($file, $line): array
+    {
+        $preview = [];
+        $lines = \abs((int) config('aegis.lines', 7));
+        $content = \file($file);
+        $max = \count($content);
+
+        for ($i = -1 * $lines; $i <= $lines; $i++) {
+            $currentLine = $line + $i;
+            $idx = $currentLine - 1;
+
+            if ($idx < 0 || $currentLine > count($content)) {
+                continue;
+            }
+
+            if (empty($content[$idx])) {
+                continue;
+            }
+
+            $preview[] = [$currentLine, $content[$idx], ];
+        }
+
+        return $preview;
+    }
+
+    public function formatException(): array
+    {
+        $preview = $this->generateCodePreview(
+            $this->exception->getFile(),
+            $this->exception->getLine(),
+        );
+
+        $formatted = [
+            'classname' => \get_class($this->exception),
+            'message' => $this->exception->getMessage(),
+            'traces' => $this->exception->getTrace(),
+            'code' => $this->exception->getCode(),
+            'file' => [
+                'name' => $this->exception->getFile(),
+                'line' => $this->exception->getLine(),
+                'hash' => \hash_file('md5', $this->exception->getFile()),
+                'preview' => $preview,
+            ],
+        ];
+
+        if ($this->cause['file'] !== $this->exception->getFile()) {
+            $causePreview = $this->generateCodePreview(
+                $this->cause['file'],
+                $this->cause['line'],
+            );
+
+            $formatted['cause'] = [
+                'name' => $this->cause['file'],
+                'line' => $this->cause['line'],
+                'hash' => \hash_file('md5', $this->cause['file']),
+                'preview' => $causePreview,
+            ];
+        }
+
+        return $formatted;
     }
 
     /**
@@ -47,35 +155,34 @@ class Record implements Arrayable
             'method' => Request::method(),
             'fullUrl' => Request::fullUrl(),
 
-            'classname' => \get_class($this->exception),
-            'message' => $this->exception->getMessage(),
-            'trace' => $this->exception->getTraceAsString(),
-            'line' => $this->exception->getLine(),
-            'code' => $this->exception->getCode(),
-            'file_name' => $this->exception->getFile(),
-            'file_hash' => \hash_file('md5', $this->exception->getFile()),
-
             'release' => config('aegis.release', null),
             'dist' => config('aegis.dist', null),
 
-            'vars_ua_string' => Request::server('HTTP_USER_AGENT'),
-            'vars_server_addr' => Request::server('SERVER_ADDR'),
-            'vars_remote_addr' => Request::server('REMOTE_ADDR'),
+            'exception' => $this->formatException(),
+
+            'variables' => [
+                'ua_string' => Request::server('HTTP_USER_AGENT'),
+                'server_addr' => Request::server('SERVER_ADDR'),
+                'remote_addr' => Request::server('REMOTE_ADDR'),
+            ],
         ];
 
         if ($user && config('aegis.user.collect', true)) {
             if ($user instanceof Aegisable) {
                 $data['user'] = $user->toAegis();
             }
-            elseif ($user instanceof Model || \method_exists($user, 'getKey')) {
-                $data['user'] = $user->getKey();
+            elseif (\is_array($user)) {
+                $data['user'] = $user;
+            }
+            elseif ($user instanceof Arrayable || \method_exists($user, 'toArray')) {
+                $data['user'] = $user->toArray();
+            }
+            elseif (\method_exists($user, 'getKey')) {
+                $data['user'] = ['id' => $user->getKey(), ];
             }
             elseif (config('aegis.user.force', true)) {
                 if ($user instanceof Jsonable) {
-                    $data['user'] = $user->toJson();
-                }
-                elseif ($user instanceof Arrayable) {
-                    $data['user'] = \json_encode($user->toArray());
+                    $data['user'] = \json_decode($user->toJson(), true);
                 }
                 else {
                     try {
